8450482e446d6b0ad21e333ed030b87683d8ea7e
gazi-allie <ibnibilal114@gmail.com>
2024-09-10 23:05:06 +0530

    video model set

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose,{Schema} from "mongoose";
+
+const VideoSchema = new Schema({ 
+    videoFile:{
+        type:String,//cloudniary url
+        required:true
+    },
+    thumbnail:{
+        type:String,//cloudinary url
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    discription:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,// CLOUDNARY
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    }
+
+    
+},{timestamps:true});
+
+
+
+export const Video=mongoose.model('Video',VideoSchema);
