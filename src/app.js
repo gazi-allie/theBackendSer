00cfa855ba3449519904b6e6faee1dc11e9077d9
gazi-allie <ibnibilal114@gmail.com>
2024-09-12 17:45:06 +0530

    all set up config

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
-import express from "express"
-import cors from  "cors"
-import cookieParser  from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+// to use the cors
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+app.use(cookieParser());
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ exdended: true, limit: "16kb" }));
 
 
-const app = express()
-// to use the cors 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true,
 
-}))
-app.use(express.json({
-    limit:"16kb"
-}))
-app.use(cookieParser())
 
-app.use (express.static("public"))
+import userRouter from "./routes/user.routs.js"
 
 
 
-app.use(express.urlencoded({exdended:true,limit:"16kb"}))
-export {app}
+
+// routs declerations
+
+app.use("/api/v1/users",userRouter)
+
+export { app };
