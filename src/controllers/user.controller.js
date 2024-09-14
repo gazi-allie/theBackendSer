064419315f3d72a0dafa2522f58aa34143e0bab9
gazi-allie <ibnibilal114@gmail.com>
2024-09-14 22:42:53 +0530

    checked on postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
-    message: "gazi the coder",
+    message: "gazi the coder lagta ha ",
   });
 });
 
