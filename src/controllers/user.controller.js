0e9db84fef8c5714759a87c42bef834f0d59660c
gazi-allie <ibnibilal114@gmail.com>
2024-09-15 22:09:20 +0530

    new change

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "gazi the coder lagta ha ",
-  });
+ //get the user details from frontend ]
+//  validation on ever detaila email, name etc (not empty)
+//  check if user already exist  check username and email 
+//  check files cover images  check for **avtar
+// upload files to cloudinary , check avatar  
+// create user object -create entry in db
+// remove password and refresh token field from responnse  
+// check for user creation 
+// return res 
+// 
+
 });
 
 export { registerUser };
